5665f3431bbf8f98201036ff8b63c00e4fe0bb73
Sasha40404 <70843523+Sasha40404@users.noreply.github.com>
2022-01-15 18:10:46 +0400

    Bean Init, Destroy, Factory method

--- src/main/java/ru/bonyachuk/springcourse/ClassicalMusic.java
+++ src/main/java/ru/bonyachuk/springcourse/ClassicalMusic.java
@@ -1,6 +1,21 @@
 package ru.bonyachuk.springcourse;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {}
+
+    // Factory method
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit() {
+        System.out.println("Doing initialization Classical Music");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing destruction Classical Music");
+    }
+
     @Override
     public String getsong() {
         return "Hungarian Rhapsody";
--- src/main/java/ru/bonyachuk/springcourse/NewSpring.java
+++ src/main/java/ru/bonyachuk/springcourse/NewSpring.java
@@ -7,20 +7,9 @@ public class NewSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-
-        System.out.println(comparison);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        firstMusicPlayer.setVolume(10);
-
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
-
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getsong());
         context.close();
     }
 }
