8da1835fc1ad9b15a59409d82223767c2b236fcc
Sasha40404 <70843523+Sasha40404@users.noreply.github.com>
2022-01-16 13:56:10 +0400

    Annotations @PostConstruct @PreDestroy

--- src/main/java/ru/bonyachuk/springcourse/ClassicalMusic.java
+++ src/main/java/ru/bonyachuk/springcourse/ClassicalMusic.java
@@ -2,6 +2,8 @@ package ru.bonyachuk.springcourse;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Component
@@ -11,6 +13,16 @@ public class ClassicalMusic implements Music {
             "Classic Music 2",
             "Classic Music 3"
     };
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization Classic Music");
+    }
+
+    @PreDestroy
+    public void doMyDesrtroy() {
+        System.out.println("Doing my destruction Classic Music");
+    }
 
     @Override
     public String getsong() {
